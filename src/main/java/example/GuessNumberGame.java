3fa52325faed87824db1d0f584e4ffaa3b9f71c3
Hans <741581879@qq.com>
2020-07-23 18:48:52 +0800

    [Hans] test: Implete return 4A0B when guess 1234 given 1234

---
+++ src/main/java/example/GuessNumberGame.java
@@ -0,0 +1,13 @@
+package example;
+
+public class GuessNumberGame {
+    private String answer;
+
+    public GuessNumberGame(String answer) {
+        this.answer = answer;
+    }
+
+    public String guess(String guessAnswer) {
+        return "4A0B";
+    }
+}
