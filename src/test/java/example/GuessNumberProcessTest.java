22fdcc51d87543e066a1017b58daf8f687ba45c8
Hans <741581879@qq.com>
2020-07-24 08:13:01 +0800

    [Hans] test: Add should_return_true_when_process_given_3_and_4A0B Test

--- src/test/java/example/GuessNumberProcessTest.java
+++ src/test/java/example/GuessNumberProcessTest.java
@@ -20,4 +20,18 @@ public class GuessNumberProcessTest {
         assertEquals(isContinueGame, false);
     }
 
+    @Test
+    void should_return_true_when_process_given_3_and_4A0B() {
+        // given
+        ProcessController controller = new ProcessController();
+        int times = 3;
+        String result = "4A0B";
+
+        // when
+        Boolean isContinueGame = controller.controlProcess(times, result);
+
+        // then
+        assertEquals(isContinueGame, true);
+    }
+
 }
