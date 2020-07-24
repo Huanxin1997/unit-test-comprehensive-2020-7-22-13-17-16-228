5edb3e60ef104cabe90f78bb40e31aa8acc1a673
Hans <741581879@qq.com>
2020-07-24 08:31:52 +0800

    [Hans] refactor: Break down the function from GuessNumberGame

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -2,6 +2,7 @@ package example;
 
 public class GuessNumberGame {
     private String answer;
+    private int guessTimes = 0;
 
     public GuessNumberGame(String answer) {
         this.answer = answer;
@@ -9,31 +10,19 @@ public class GuessNumberGame {
 
     public String guess(String guessAnswer) {
         if(!guessAnswer.equals(answer)) {
-            return generateOutputResult(guessAnswer);
+            return generateOutputResult(guessAnswer, answer);
         }
         return "4A0B";
     }
 
-    public int[] validAnswer(String guessAnswer) {
-        System.out.println(guessAnswer);
-        System.out.println(this.answer);
-        int errorPositionCount = 0;
-        int rightPositionCount = 0;
-        for(int i = 0;i < guessAnswer.length();i ++) {
-            if(guessAnswer.charAt(i) == answer.charAt(i)) {
-                rightPositionCount ++;
-            }else{
-                if(this.answer.contains(String.valueOf(guessAnswer.charAt(i)))) {
-                    errorPositionCount ++;
-                }
-            }
-        }
-        int[] result = new int[]{rightPositionCount, errorPositionCount};
+    String generateOutputResult(String guessAnswer, String answer) {
+        GeneratorUtil util = new GeneratorUtil();
+        String result = util.generateOutputResult(guessAnswer, answer);
         return result;
     }
 
-    String generateOutputResult(String guessAnswer) {
-        int[] resultNums = validAnswer(guessAnswer);
-        return String.format("%sA%sB", resultNums[0], resultNums[1]);
+    Boolean calculateTimes() {
+        guessTimes ++;
+        return guessTimes > 6;
     }
 }
